d7d6f270b21ed60f9725a553edd5ebf0bd2e3476
Nonrapak <56886497+TeerapongP@users.noreply.github.com>
2021-10-21 19:30:54 +0700

    first-commit

--- Project_Business/Project_Business/Pages/Privacy.cshtml.cs
+++ Project_Business/Project_Business/Pages/Privacy.cshtml.cs
@@ -6,20 +6,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication1.Pages
+namespace Project_Business.Pages
 {
-    public class IndexModel : PageModel
+    public class PrivacyModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<PrivacyModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
         public void OnGet()
         {
-
         }
     }
 }
--- Project_Business/Project_Business/Startup.cs
+++ Project_Business/Project_Business/Startup.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication1
+namespace Project_Business
 {
     public class Startup
     {
